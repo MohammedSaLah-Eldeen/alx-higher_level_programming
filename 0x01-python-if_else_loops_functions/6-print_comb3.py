57d3224fbeba80f11237bcb9c1933dd7f338706f
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-09-05 19:19:32 +0000

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for n1 in range (8):
-    for n2 in range (n1 + 1, 10):
+for n1 in range(8):
+    for n2 in range(n1 + 1, 10):
         print("{}{}, ".format(n1, n2), end="")
 print("{}{}".format(n1 + 1, n2))
