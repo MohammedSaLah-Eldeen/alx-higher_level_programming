58fc3338416c1d1810b877a22d6e00446a046cd0
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-10-02 21:48:39 +0000

    Task 7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -1,15 +1,4 @@
-ntation.
-    """
-
-    number_of_instances = 0
-    print_symbol = "#"
-
-    def __init__(self, width=0, height=0):
-        """Initialize a new Rectangle.
-
-            Args:
-                        width (int): The width of the new rectangle.
-                                    height (int): The he#!/usr/bin/python3
+#!/usr/bin/python3
 """Defines a Rectangle class."""
 
 
@@ -18,7 +7,18 @@ class Rectangle:
 
     Attributes:
         number_of_instances (int): The number of Rectangle instances.
-        print_symbol (any): The symbol used for string represeight of the new rectangle.
+        print_symbol (any): The symbol used for string representation.
+    """
+
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
         """
         type(self).number_of_instances += 1
         self.width = width
