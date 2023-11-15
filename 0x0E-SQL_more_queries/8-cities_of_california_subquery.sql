a01f1f9ceab88a0aad868cb12d3a8afcda7cdd18
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-11-15 20:58:54 +0000

    Task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,9 @@
 -- task 8
-SELECT cities.id, cities.name
-FROM cities, states
-WHERE states.name = 'California'
-ORDER BY cities.id
+SELECT id, name
+FROM cities
+WHERE state_id = (
+      SELECT id
+      FROM states
+      WHERE name = 'California'
+)
+ORDER BY id;
