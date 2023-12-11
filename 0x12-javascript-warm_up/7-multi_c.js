314d9446fbedf3050173e4412616830f74681845
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-12-11 19:59:36 +0000

    Task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+if (process.argv[2] === undefined || isNaN(parseInt(process.argv[2]))) console.log('Missing number of occurrences');
+else {
+  for (let c = 0; c <= parseInt(process.argv[2]); c++) {
+    console.log('C is fun');
+  }
+}
