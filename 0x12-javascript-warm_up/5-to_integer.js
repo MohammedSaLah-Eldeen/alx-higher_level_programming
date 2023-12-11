ccc984f3e47936fc47465aac1485abec20c5c3e1
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-12-11 19:07:14 +0000

    Task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
 let parsedNumber = parseInt(process.argv[2]);
-if (!parsedNumber) console.log('Not a Number');
-else console.log(parsedNumber);
+if (isNaN(parsedNumber) || parsedNumber === undefined) console.log('Not a Number');
+else console.log('My number: ' + parsedNumber);
