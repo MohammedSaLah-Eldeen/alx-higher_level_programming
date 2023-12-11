e89d15fd0baeb4118ac83797051b1af2aebf267d
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-12-11 20:28:01 +0000

    Task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+function factorial (number) {
+  if (isNaN(number) || number === 1) return 1;
+
+  return number * factorial(number - 1);
+}
+
+const fact = factorial(parseInt(process.argv[2]));
+console.log(fact);
+
