77b6bbe8de97c9b76ee797d1addea2b04e2d469a
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-12-11 20:19:40 +0000

    Task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 function add(a, b) {
-  let res = a + b;
+  const res = a + b;
   console.log(res);
 }
 
-add(process.argv[2], process.argv[3]);
+add(parseInt(process.argv[2]), parseInt(process.argv[3]));
