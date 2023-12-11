4a3a0ed4dc5d4a3060ea85f64786e2f3a8f66c0f
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-12-11 19:27:25 +0000

    Task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+let adjectives = ['fun', 'cool', 'amazing'];
+let langs = ['C', 'Python', 'JavaScript'];
+
+for (let idx = 0; idx < langs.length; idx++)
+{
+    console.log(langs[idx], 'is', adjectives[idx]);
+}
