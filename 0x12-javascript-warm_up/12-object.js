806fccf9e6ce4cc1d2e63b907b8544032a1d414e
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-12-11 20:47:59 +0000

    Task 11

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -2,6 +2,7 @@
 
 if (process.argv.length <= 3) console.log('0');
 else {
-  const list = process.argv.slice(2).map(Number);
-  console.log(list.sort()[-2]);
+  const arr = process.argv.slice(2).map(Number).sort().pop();
+  const second = arr[-1];
+  console.log(second);
 }
