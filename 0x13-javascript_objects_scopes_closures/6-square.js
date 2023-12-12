79b37e404c80031f38199c35b8fabeb345f735fb
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-12-12 22:54:42 +0000

    Task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,19 +1,12 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const Square_ = require('./5-square');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends Square_ {
   charPrint (c) {
     if (c === undefined) c = 'X';
-
     for (let i = 1; i <= this.width; i++) {
       console.log(c.repeat(this.width));
     }
   }
 }
-
-module.exports = Square;
