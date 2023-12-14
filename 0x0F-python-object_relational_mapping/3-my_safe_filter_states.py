f5fe7b1f9c6e661a236ebf6ff0e6e4bbcd9bba9f
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-12-14 15:40:14 +0000

    Task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Task 3.
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    usrname = sys.argv[1]
+    passwd = sys.argv[2]
+    dbname = sys.argv[3]
+    state_name_searched = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=usrname,
+        passwd=passwd,
+        db=dbname
+    )
+
+    cur = db.cursor()
+    cur.execute(
+        '''
+        SELECT * 
+        FROM states
+        WHERE BINARY states.name = %s
+        ORDER BY states.id
+        ''',
+        (state_name_searched,)
+    )
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
