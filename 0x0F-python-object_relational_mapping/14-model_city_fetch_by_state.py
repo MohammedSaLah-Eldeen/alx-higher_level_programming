00e5a16ab1be83308a0081cdb6704e24de426742
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-12-15 18:58:40 +0000

    Task 14

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Task 12.
+"""
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    usrname = sys.argv[1]
+    passwd = sys.argv[2]
+    dbname = sys.argv[3]
+
+    engine = create_engine(
+      'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(usrname, passwd, dbname)
+    )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = (
+        session
+        .query(
+            City.id,
+            City.name.label('city_name'),
+            State.name.label('state_name')
+        )
+        .join(City, on=State.id == City.state_id)
+        .all()
+    )
+
+    for record in result:
+        print('{}: ({}) {}'.format(record.state_name, record.id, record.city_name))
